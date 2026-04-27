d62024fd63769ce89e28038681e093b5c2edb1d4
muhammadfadhil21 <muhammadfadhil.jens@gmail.com>
2026-04-27 23:49:36 +0700

    Update_Payment2

--- src/components/CheckoutModal.tsx
+++ src/components/CheckoutModal.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { X, Upload, CheckCircle2, Loader2, ArrowRight } from "lucide-react";
 import { supabase } from "@/lib/supabase";
-import type { Product } from "@/data/products"; // Pastikan path import ini benar
+import { Product } from "@/data/products"; // Pastikan path import ini benar
 
 type CheckoutModalProps = {
   product: Product;
